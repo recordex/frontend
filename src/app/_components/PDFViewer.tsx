aa2ddf6a7f81f3fcd60653006eecd44ee5fb1c3a
Keisuke Ishigami <ishigami_keisuke@cyberagent.co.jp>
2024-02-15 12:16:25 +0900

    chore: pdf を見やすくしました

--- src/app/_components/PDFViewer.tsx
+++ src/app/_components/PDFViewer.tsx
@@ -2,10 +2,11 @@
 
 import { useState } from 'react';
 import { pdfjs, Document, Page } from 'react-pdf';
+import { Button } from '@mui/material';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-// 参考: https://github.com/wojtekmaj/react-pdf/issues/136#issuecomment-818728828
+// PDF.jsのワーカーの設定
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 interface PDFViewerProps {
@@ -14,21 +15,55 @@ interface PDFViewerProps {
 
 const PDFViewer: React.FC<PDFViewerProps> = ({ file }) => {
   const [numPages, setNumPages] = useState<number | null>(null);
+  const [pageNumber, setPageNumber] = useState(1); // 現在のページ番号
 
   const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages);
   };
 
+  const nextPage = () => {
+    if (numPages === null) return; // numPagesがnullなら何もしない
+    setPageNumber((prevPageNumber) =>
+      prevPageNumber < numPages ? prevPageNumber + 1 : prevPageNumber,
+    );
+  };
+
+  const previousPage = () => {
+    if (numPages === null) return; // numPagesがnullなら何もしない
+    setPageNumber((prevPageNumber) =>
+      prevPageNumber > 1 ? prevPageNumber - 1 : prevPageNumber,
+    );
+  };
+
   return (
-    <Document
-      file={file}
-      onLoadSuccess={onDocumentLoadSuccess}
-      options={{ cMapUrl: '/cmaps/', cMapPacked: true }}
-    >
-      {Array.from(new Array(numPages), (el, index) => (
-        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-      ))}
-    </Document>
+    <>
+      <Document
+        file={file}
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={{ cMapUrl: '/cmaps/', cMapPacked: true }}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <div>
+        <Button
+          onClick={previousPage}
+          disabled={pageNumber <= 1}
+          variant='outlined'
+          sx={{ mr: 4 }}
+        >
+          前のページ
+        </Button>
+        ページ {pageNumber} / {numPages}
+        <Button
+          onClick={nextPage}
+          disabled={pageNumber >= (numPages ?? 0)}
+          variant='outlined'
+          sx={{ ml: 4 }}
+        >
+          次のページ
+        </Button>
+      </div>
+    </>
   );
 };
 
